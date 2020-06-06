a436b82a685ed29b5fad8dedcce8e55ded60b113
vivek6219 <33075276+vivek6219@users.noreply.github.com>
2020-06-06 16:20:57 -0700

    What i changed

--- TetrisJS/app.js
+++ TetrisJS/app.js
@@ -1,3 +1,9 @@
-var x = 1;
-x=23;
-console.log(x);
+document.addEventListener('DOMContentLoaded', () =>{
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const grid = document.querySelector('.grid');
+    const scoreDisplay = document.querySelector('#score');
+    const startBtn = document.querySelector("#start-button")
+    const width =10;
+
+    console.log(squares);
+})
