21588d059546e099f6985251b86561ec4cf068cf
vivek6219 <33075276+vivek6219@users.noreply.github.com>
2020-06-07 17:04:38 -0700

    Late night Changes

--- TetrisJS/app.js
+++ TetrisJS/app.js
@@ -5,5 +5,63 @@ document.addEventListener('DOMContentLoaded', () =>{
     const startBtn = document.querySelector("#start-button")
     const width =10;
 
-    console.log(squares);
+//Tetrminoes
+const lTetromino =[
+    [1, width+1, width*2+1,2],
+    [width, width +1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+];
+
+const zTetromino=[
+    [0,width, width+1, width*2+1],
+    [width+1, width+2,width*2, width*2+1],
+    [0, width,width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1]
+];
+
+const tTetrimino =[
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2, width*2+1],
+    [1,width,width+1,width+2]
+
+];
+const oTetromino=[
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+];
+
+const iTetromino=[
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2,width+3],
+    [1, width+1, width*2+1, width*3+1]
+    [width, width+1, width+2,width+3]
+];
+
+const theTetrimnoes=[lTetromino,zTetromino,tTetrimino, oTetromino, iTetromino];
+
+let currentPosition =4;
+let currnetRotation =0;
+
+//randomly select a tetromino
+let random = Math.floor(Math.random()*theTetrimnoes.length);
+let current = theTetrimnoes[0][0];
+
+//draw the  tetromino
+function draw()
+{
+    current.forEach(index =>{
+        squares[currentPosition +index].classList.add('tetromino');
+    });
+}
+
+function undraw(){
+    current.forEach(index=>{
+        squares[currentPosition+index].classList.remove('tetromino');
+    });
+}
+
 })
