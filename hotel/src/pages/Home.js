86a3028d860075dfdb0b6d07ee9f1112c3d1c580
vivek6219 <33075276+vivek6219@users.noreply.github.com>
2020-06-15 20:32:33 -0700

    DATA API

---
+++ hotel/src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Hero from '../components/Hero'
+const Home =() =>{
+    return <Hero hero ="defaultHero"> </Hero>
+}
+
+export default Home
---
+++ hotel/src/pages/SingleRooms.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+export default class SingleRooms extends Component {
+
+    render()
+    {
+        return <div> hello from Single Rooms in your area nearby</div>;
+    }
+}
+
